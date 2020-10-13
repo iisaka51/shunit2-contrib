468a7fc14802264b9169097cc7dae7a56d6cfe17
Goichi Iisaka <iisaka51@gmail.com>
2020-10-13 14:21:39 +0900

    support color

--- shunit2_functions.sh
+++ shunit2_functions.sh
@@ -54,3 +54,38 @@ is_executable() {
     # FILE exists and is executable
     test -x ${_FILE}
 }
+
+# ----------------------------------
+# Colors
+# ----------------------------------
+ESC=$(printf '\033')
+RED="${ESC}[0;31m"
+GREEN="${ESC}[0;32m"
+ORANGE="${ESC}[0;33m"
+BLUE="${ESC}[0;34m"
+PURPLE="${ESC}[0;35m"
+CYAN="${ESC}[0;36m"
+LIGHTGRAY="${ESC}[0;37m"
+DARKGRAY="${ESC}[1;30m"
+LIGHTRED="${ESC}[1;31m"
+LIGHTGREEN="${ESC}[1;32m"
+YELLOW="${ESC}[1;33m"
+LIGHTBLUE="${ESC}[1;34m"
+LIGHTPURPLE="${ESC}[1;35m"
+LIGHTCYAN="${ESC}[1;36m"
+WHITE="${ESC}[1;37m"
+NOCOLOR="${ESC}[0m"
+
+ECHO() {
+# Usage:
+#    ECHO $RED "This is ERROR"
+#    ECHO "" "This is normal string"
+#    ECHO -- "This is normal string"
+
+    if [ "$1" == "--" -o "$1" == "" ] ; then
+        unset _COLOR ; shift
+    else
+        _COLOR=$1 ; shift
+    fi
+    echo ${_COLOR} "$@" ${NOCOLOR}
+}
